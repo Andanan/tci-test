84dcbdd6d3350897a390504a33e502924cea9bd0
Andanan <Andanan@mail.de>
2017-08-21 23:57:49 +0200

    Updated 'after_success' script to test if mail or mutt are installed and if yes try to send a mail

--- src/main/scripts/package-send.sh
+++ src/main/scripts/package-send.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 # date: 21.08.2017
 # author: Andanan
+PROJECT_ROOT=$(pwd)
 PROJECT_NAME="tci-test-0.0.1-SNAPSHOT"
-ARCHIVE_NAME="$PROJECT_NAME.tar.gz"
+ARCHIVE_REL="target/$PROJECT_NAME-test.tar.gz"
+ARCHIVE="$PROJECT_ROOT/$ARCHIVE_REL"
+TARGET_DIR="$PROJECT_ROOT/target"
+DIST_DIR="$TARGET_DIR/dist"
 RECIPIENT="andanan@mail.de"
-echo "--- packaging '$ARCHIVE_NAME' ---"
-cd target/dist
-tar -czf ../$ARCHIVE_NAME *
-cd ..
-echo "--- sending $ARCHIVE_NAME to $RECIPIENT"
-tar -tvf $ARCHIVE_NAME
-cd ..
-pwd
+
+echo "--- packaging '$ARCHIVE_REL' ---"
+cd $DIST_DIR
+tar -czf $ARCHIVE *
+cd $TARGET_DIR
+echo "--- verifying archive content ---"
+tar -tvf $ARCHIVE
+cd $PROJECT_ROOT
+
+echo "--- sending $ARCHIVE_REL to $RECIPIENT ---"
+function isInstalled() {
+	if command $1 2>/dev/null; then
+		echo "$1 is installed"
+		$2
+	else
+		echo "$1 is not installed. Aborting!"
+	fi
+}
+
+isInstalled mail "echo \"MSG\" | mail -s \"subj\" -a \"$ARCHIVE\" -- $RECIPIENT"
+isInstalled mutt "echo \"MSG\" | mutt -s \"subj\" -a \"$ARCHIVE\" $RECIPIENT"
+
+echo "--- end ---"
